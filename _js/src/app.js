3eec2774f9de6e42f75ca53ed5981e0bb6ee4008
andreadelrio <delrio.andre@gmail.com>
2017-12-22 09:59:35 -0800

    updates to the slider

--- _js/src/app.js
+++ _js/src/app.js
@@ -93,5 +93,29 @@ $('.image-link').magnificPopup({
 });
 
 /*slick*/
-$('.your-class').slick();
+// $('.your-class').slick({
+//   autoplay: true,
+//   // cssEase: fade;
+// });
+var $status = $('.pagingInfo');
+var $slickElement = $('.your-class');
 
+
+$slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text(i + ' of ' + slick.slideCount);
+});
+
+$slickElement.slick({
+    // slide: 'img',
+    draggable: false,
+    autoplay: true,
+    appendArrows: $('.hero__nav'),
+    prevArrow: $('.js-hero-prev'),
+    nextArrow: $('.js-hero-next'),
+    dots: false,
+    speed: 700,
+    fade: true,
+    cssEase: 'linear'
+});
